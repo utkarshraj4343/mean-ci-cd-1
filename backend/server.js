e38a0e2ae003be016d1e7d40bd4f00a9528fcfe5
Utkarsh Raj <itsUtkarsh@gmail.com>
2025-09-19 12:01:44 +0530

    Add Docker and Docker Compose configuration

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res)=>{
 });
 
 app.listen(port,()=>{
-    console.log('Server is running on port : ${port}');
+    console.log(`Server is running on port : ${port}`);
     
 })
 
